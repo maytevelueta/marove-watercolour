d160f1b3fa47bacbb5f793dea8295cf6d537da08
Mayte Velueta <maytevelueta@gmail.com>
2025-08-20 23:31:33 +0100

    2008

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Carousel from '../components/Carousel';
+import '../index.css';
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <section className="section fade-in">
-        <h2>Welcome to Marove Aqua</h2>
-        <p>Your minimalistic watercolour portfolio.</p>
+      <section className="welcome-section">
+  <div className="home-title">
+    <h1 className="title">Welcome</h1>
+    <h1 className="title">to</h1>
+    <h1 className="title">my</h1>
+    <h1 className="title">virtual</h1>
+    <h1 className="title">gallery</h1>
+  </div>
       </section>
 
       <section className="section fade-in">
