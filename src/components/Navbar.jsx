0a9327e9dc0b19349df954ee3feff8215b7b8dc3
Mayte Velueta <maytevelueta@gmail.com>
2025-08-26 22:20:33 +0100

    2608

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">Marove Aqua</div>
+      <div className="logo">
+        <Link to="/">Marove Aqua</Link>
+      </div>
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/gallery">Gallery</Link></li>
