67a3bd38b68653a5fc22c68eda7e26fb7be88f79
Mayte Velueta <maytevelueta@gmail.com>
2025-08-27 20:51:35 +0100

    2708

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../assets/logo-black.png"; // adjust path if needed
 
 export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="logo">
-        <Link to="/">Marove Aqua</Link>
+        <Link to="/">
+          <img src={logo} alt="Marove Aqua Logo" className="logo-img" />
+        </Link>
       </div>
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
