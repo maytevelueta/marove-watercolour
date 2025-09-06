cf0b5d621f79a6112bc7bee418498c3e4446938d
Mayte Velueta <maytevelueta@gmail.com>
2025-09-06 11:22:04 +0100

    0609

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,9 +7,9 @@ export default function About() {
       <main className="about-section">
         <h2>About</h2>
         <p>Hi, I'm Mayte, a watercolour artist from Mexico. I started Marove Aqua in early 2021 as a small personal project to bring colour and life to my flat. Discovering this medium was a truly transformative experience for me.</p>
-        <br/>
+       
         <p>My work focuses on botanical illustrations and animal subjects, capturing the delicate details and vibrant textures of the natural world. I'm constantly exploring new techniques and seeking inspiration from nature.</p>
-        <br/>
+        
         <p>If you have any enquiries or would like to commission a custom painting, please don’t hesitate to get in touch—I'd be delighted to create something unique for you! 🙂
         </p>
       </main>
