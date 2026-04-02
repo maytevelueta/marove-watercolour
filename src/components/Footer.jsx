979e34e159c9a9ce98fe92a29effadb79bf69bf7
Mayte Velueta <maytevelueta@gmail.com>
2026-04-02 09:16:38 +0100

    0204

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaInstagram } from "react-icons/fa";
+import { FaInstagram, FaFacebookF, FaEtsy } from "react-icons/fa";
 import "../index.css";
 
 export default function Footer() {
@@ -8,15 +8,34 @@ export default function Footer() {
       <div className="footer-inner">
         <p>© {new Date().getFullYear()} Marove Aqua. All rights reserved.</p>
 
-        <a
-          href="https://www.instagram.com/"
-          target="_blank"
-          rel="noreferrer"
-          aria-label="Visit Instagram"
-          className="footer-social"
-        >
-          <FaInstagram />
-        </a>
+        <div className="footer-social">
+          <a
+            href="https://www.instagram.com/marove_aqua/"
+            target="_blank"
+            rel="noreferrer"
+            aria-label="Visit Instagram"
+          >
+            <FaInstagram />
+          </a>
+
+          <a
+            href="https://www.etsy.com/shop/MaroveAqua?ref=dashboard-header"
+            target="_blank"
+            rel="noreferrer"
+            aria-label="Visit Etsy"
+          >
+            <FaEtsy />
+          </a>
+
+          <a
+            href="https://www.facebook.com/maroveaqua/"
+            target="_blank"
+            rel="noreferrer"
+            aria-label="Visit Facebook"
+          >
+            <FaFacebookF />
+          </a>
+        </div>
       </div>
     </footer>
   );
